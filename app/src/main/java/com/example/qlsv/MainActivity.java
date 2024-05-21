b208826a904c1da5bbedbbe11b3be3ca538d6f67
Duchai07200103 <duchai298@gmail.com>
2024-05-21 10:41:55 +0700

    src

--- app/src/main/java/com/example/qlsv/MainActivity.java
+++ app/src/main/java/com/example/qlsv/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.qlsv;
 
+import android.annotation.SuppressLint;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +14,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    EditText edtmalop, edttenlop, edtsiso;
+    Button btninsert, btndelete, btnupdate, btnquery;
+
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
+    SQLiteDatabase mydatabase;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtmalop = findViewById(R.id.edtmalop);
+        edttenlop = findViewById(R.id.edttenlop);
+        edtsiso = findViewById(R.id.edtsiso);
+        btninsert = findViewById(R.id.btninsert);
+        btndelete = findViewById(R.id.btndelete);
+        btnquery = findViewById(R.id.btnquery);
+        btnupdate = findViewById(R.id.btnupdate);
+
+        lv = findViewById(R.id.lv);
+
     }
 }
